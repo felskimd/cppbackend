1cdbc40a4e0b5d31ec3bf9f1c13e5cee035560b7
Felskimd <gilmanshin.roman@yandex.ru>
2025-03-11 06:57:49 +0300

    sprint 2 logger

---
+++ sprint2/problems/logger/solution/my_logger.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <optional>
+#include <mutex>
+#include <thread>
+
+using namespace std::literals;
+
+#define LOG(...) Logger::GetInstance().Log(__VA_ARGS__)
+
+class Logger {
+    auto GetTime() const {
+        if (manual_ts_) {
+            return *manual_ts_;
+        }
+
+        return std::chrono::system_clock::now();
+    }
+
+    auto GetTimeStamp() const {
+        const auto now = GetTime();
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
+        return std::put_time(std::localtime(&t_c), "%F %T");
+    }
+
+    // Для имени файла возьмите дату с форматом "%Y_%m_%d"
+    std::string GetFileTimeStamp() const {
+        const auto now = GetTime();
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
+        char mbstr[10];
+        std::strftime(mbstr, 10, "%F", std::localtime(&t_c));
+        std::string date(mbstr);
+        return "/var/log/sample_log_" + date + ".log";
+    }
+
+    Logger() = default;
+    Logger(const Logger&) = delete;
+
+public:
+    static Logger& GetInstance() {
+        static Logger obj;
+        return obj;
+    }
+
+    // Выведите в поток все аргументы.
+    template<class... Ts>
+    void Log(const Ts&... args) {
+        std::lock_guard<std::mutex> lock(m_);
+        stream_.open(GetFileTimeStamp(), std::ios::app);
+        stream_ << GetTimeStamp << ": ";
+        LogImpl(stream_, args...);
+        stream_ << std::endl;
+    }
+
+    // Установите manual_ts_. Учтите, что эта операция может выполняться
+    // параллельно с выводом в поток, вам нужно предусмотреть 
+    // синхронизацию.
+    void SetTimestamp(std::chrono::system_clock::time_point ts) {
+        std::lock_guard<std::mutex> lock(m_);
+        manual_ts_ = ts;
+    }
+
+private:
+    std::optional<std::chrono::system_clock::time_point> manual_ts_;
+    std::mutex m_;
+    std::ofstream stream_;
+
+    template <typename T0, typename... Ts>
+    void LogImpl(std::ofstream& out, const T0& v0, const Ts&... vs) {
+        out << v0;
+    }
+};
