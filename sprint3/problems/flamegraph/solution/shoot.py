40d4503389194ab56848bedb9fb80e7229448046
Felskimd <gilmanshin.roman@yandex.ru>
2025-04-07 17:27:26 +0300

    sprint 3 flamegraph

---
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -0,0 +1,58 @@
+import argparse
+import subprocess
+import time
+import random
+import shlex
+
+RANDOM_LIMIT = 1000
+SEED = 123456789
+PERF_COMMAND = 'sudo perf record -p'
+GRAPH_COMMAND = 'sudo perf script | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl > graph.svg'
+random.seed(SEED)
+
+AMMUNITION = [
+    'localhost:8080/api/v1/maps/map1',
+    'localhost:8080/api/v1/maps'
+]
+
+SHOOT_COUNT = 100
+COOLDOWN = 0.1
+
+
+def start_server():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('server', type=str)
+    return parser.parse_args().server
+
+
+def run(command, output=None):
+    process = subprocess.Popen(shlex.split(command), stdout=output, stderr=subprocess.DEVNULL)
+    return process
+
+
+def stop(process, wait=False):
+    if process.poll() is None and wait:
+        process.wait()
+    process.terminate()
+
+
+def shoot(ammo):
+    hit = run('curl ' + ammo, output=subprocess.DEVNULL)
+    time.sleep(COOLDOWN)
+    stop(hit, wait=True)
+
+
+def make_shots():
+    for _ in range(SHOOT_COUNT):
+        ammo_number = random.randrange(RANDOM_LIMIT) % len(AMMUNITION)
+        shoot(AMMUNITION[ammo_number])
+    print('Shooting complete')
+
+
+server = run(start_server())
+run([PERF_COMMAND, server.pid, 'sleep 10'])
+make_shots()
+stop(server)
+time.sleep(1)
+run(GRAPH_COMMAND)
+print('Job done')
