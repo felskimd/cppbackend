4542846df3f8cab103f234d9a70e92646633954c
Felskimd <gilmanshin.roman@yandex.ru>
2025-04-07 21:30:06 +0300

    sprint 3 flamegraph

--- sprint3/problems/flamegraph/solution/shoot.py
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -60,7 +60,8 @@ stop(perf, True)
 graph = run(GRAPH_COMMAND)
 graph.wait()
 if graph.returncode != 0:
-    print("Error:", graph.stderr.read().decode())
+    print("Error:")
+    print(graph.stderr.read())
 else:
     print("Flamegraph created")
 print(os.path.getsize('perf.data'))
