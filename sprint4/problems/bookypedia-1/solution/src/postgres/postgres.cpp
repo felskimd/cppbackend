28bbd393345ec5269531c6c542afebc25078d52a
Felskimd <gilmanshin.roman@yandex.ru>
2025-05-08 18:34:35 +0300

    sprint 4 bookipedia-1

--- sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
+++ sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
@@ -90,7 +90,7 @@ CREATE TABLE IF NOT EXISTS authors (
     work.exec(R"(
 CREATE TABLE IF NOT EXISTS books (
     id UUID CONSTRAINT book_id_constraint PRIMARY KEY,
-    author_id UUID CONSTRAINT author_id_constraint NOT NULL,
+    author_id UUID NOT NULL,
     title varchar(100) NOT NULL,
     publication_year integer
 );
